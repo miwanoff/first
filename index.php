f68d2c3eb2302a243c2e0af20b2645b6d82f053c
miwanoff <marina.iva.noffa@gmail.com>
2020-04-28 18:57:59 +0300

    Update index.php
    Changing to poem.php

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
-<?php 
-$x = 5;
-$y = 3;
-$z = 8;
-echo $x + $y;
+<?php
+$file = fopen("poem.txt", "r");
+$arr = [];
+// while(!feof($file)) 
+// { 
+//     $arr[] = fgets($file); 
+// }
+
+$arr = file("poem.txt") ;
+$path = "path_poem";
+if (!is_dir($path)) {
+    mkdir($path);
+   }
+//print_r($arr);
+
+$str_b = '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>';
+$str_e = '</body>
+</html>';
+
+$p = fopen($path."/poem.html", "w");
+fwrite($p, $str_b);
+//fwrite($p, "1111");
+foreach($arr as $i => $a){
+    if($i==0){
+        fwrite($p, "$a<br />");
+
+    }
+    else
+    fwrite($p, "$a<br />");
+}
+fwrite($p, $str_e);
